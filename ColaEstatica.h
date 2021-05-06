81d77945a265290c97507c0ba7e9011f492a985a
agustina <amonteromilano@gmail.com>
2021-05-06 02:00:37 -0300

    cambios listas02 y colaestatica

--- ColaEstatica.h
+++ ColaEstatica.h
@@ -1,37 +1,31 @@
-// Cola Circular con Arreglo.cpp : 
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include "Listas02.h"
 
-#define MAX 101
+#define MAX 101                             //La cola almacena MAX-1 elementos
 
 using namespace std;
 
 class ColaDePilas {
     private:
-        Pila* Q[MAX];
+        Pila* pilas[MAX];
         int frente, ult;
     public:
         ColaDePilas(void) { frente = 0; ult = MAX - 1; };
         ~ColaDePilas(void);
-        Pila* tope() { return Q[frente]; };
+        Pila* tope() { return pilas[frente]; };
         bool colavacia() { return frente == (ult + 1) % MAX; };
         bool colallena() { return frente == (ult + 2) % MAX; };
         void encolar(Pila* a);
         void desencolar();
-        Pila* ultimo();
         void imprimir();
     };
 
-
-//-------- Metodos de la Cola de Pilas --------------------
 void ColaDePilas::encolar(Pila* a){
     if (!colallena()) {
-        //if (ult == MAX - 2) ult = MAX - 1;
         ult = (ult + 1) % MAX;
-        Q[ult] = a;
-      //  cout<<"Encole correctamente" <<endl;
+        pilas[ult] = a;
     }
     else cout << "Cola llena" << endl;
 }
@@ -41,15 +35,10 @@ void ColaDePilas::desencolar(void){
     else cout << "Cola vacia" << endl;
 }
 
-Pila* ColaDePilas::ultimo(void){
-    return Q[ult];
-}
-
 void ColaDePilas::imprimir(){
     if (!colavacia()) {
-       for (int i = frente; i != (ult + 1) % MAX; i = (i + 1) % MAX){
-            //cout<<"Llamo a impre de pila" <<endl;
-            Q[i]->imprimir();
+       for (int i=0; i<MAX-1; i++){
+            pilas[i]->imprimir();
        }
     }else{
         cout<<"La cola esta vacia" <<endl;
--- Listas02.h
+++ Listas02.h
@@ -29,30 +29,15 @@ class Lista {
         void add(int x, int y);
         bool esvacia(void);
         Nodo* cabeza(void);
-        Lista* resto(void);
-        string toPrint(string p);
         void impre(void);
-        int size();
-        Nodo* last();
-        void borrarDato(int x, int y);
         void borrar(void);                      //Borra la cabeza
-        void borrar_last();                     //Borra el ultimo
-        void tomar(int n);                      //deja vivos los n primero nodos y borra el resto
 };
 
-void Lista::add(int a, int b) //100
+void Lista::add(int a, int b)
 {
     Nodo* nuevo = new Nodo(a, b);
     nuevo->set_next(czo);
     czo = nuevo;
-}
-
-Nodo* Lista::last()
-{
-    if (!this->esvacia()) {
-        if (this->resto()->esvacia())return this->cabeza();
-        return this->resto()->last();
-    }return 0;
 }
 
 bool Lista::esvacia(void)
@@ -69,56 +54,12 @@ Nodo* Lista::cabeza(void)
     return czo;
 }
 
-Lista* Lista::resto(void)
-{
-    Lista* l = new Lista(czo->get_next());
-    return (l);
-}
-
-string Lista::toPrint(string p)
-{
-    if (this->esvacia()) {
-        return p;
-    }
-    else {
-        ostringstream stm;
-        stm << this->cabeza()->get_col() <<"-" <<this->cabeza()->get_fila() << "/" << this->resto()->toPrint(p) << endl;
-        return stm.str();
-    }
-}
-
-int Lista::size()
-{
-    if (this->esvacia()) return 0;
-    return 1 + this->resto()->size();
-}
-
-void Lista::borrar(void)
-{ //borra el nodo cabeza
+void Lista::borrar(void)	//borra el nodo cabeza
+{ 
     if (!this->esvacia()) {
         Nodo* tmp = czo;
         czo = czo->get_next();
         delete tmp;
-    }
-}
-
-void Lista::borrar_last()
-{ // borra el ultimo nodo
-    if (!this->esvacia()) {
-        if ((czo->get_next())->get_next() == NULL) {
-            delete czo->get_next();
-            czo->set_next(NULL);
-        }
-        else this->resto()->borrar_last();
-    }
-}
-
-
-void Lista::tomar(int n)
-{ //deja "vivos" los n primeros nodos y borra el resto
-    if (this->size() > n) {
-        this->borrar_last();
-        this->tomar(n);
     }
 }
 
